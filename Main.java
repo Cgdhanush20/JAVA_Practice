7be86fd88baa2f20843b55d244f03ae6417009d6
Cgdhanush20 <95674712+Cgdhanush20@users.noreply.github.com>
2024-06-13 17:49:51 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+package App;
+import math.*;
+public class Main 
+{
+    public static void main(String[] args) {
+        Calculator c=new Calculator();
+        System.out.println(c.add(1, 2));
+        System.out.println(c.sub(1, 2));
+        System.out.println(c.mul(1, 6));
+        System.out.println(c.div(4, 2));
+    }
+}
